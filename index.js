35824117b73dfb88a8ed1eeb76d54468de41a98d
Osmar <juninhomachado579@gmail.com>
2024-06-13 09:54:30 -0500

    feat: resposta ao instrumento escolhido (incompleta)

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+let instrumentCorda = "violão";
+let instrumentSopro = "trompete";
+let instrumentpercuss = "bateria";
+
 console.log("Você toca algum instrumento?");
 
 process.stdin.on("data", function(data){
     let entrada = data.toString().trim();
-    if(entrada == "sim" || entrada == "s"){
-
-    }
-
-    if(entrada == não || entrada == "nao" || entrada == "n"){
+    
+    if(entrada == "não" || entrada == "nao" || entrada == "n"){
         console.log("Deveria aprender");
+        process.exit();
+    }else{
+        console.log("Qual?");
+        
     }
+    
+    
+        
 
 });
+
