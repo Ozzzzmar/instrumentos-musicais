3855ca0ab7c130e0f86de6093f64bc1f18a2b335
Osmar <juninhomachado579@gmail.com>
2024-06-13 10:29:34 -0500

    feat: resposta ao instrumento adicionada, mas sujeita a alterações

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let instrumentCorda = "violão";
-let instrumentSopro = "trompete";
-let instrumentpercuss = "bateria";
-
 console.log("Você toca algum instrumento?");
 
-process.stdin.on("data", function(data){
-    let entrada = data.toString().trim();
-    
-    if(entrada == "não" || entrada == "nao" || entrada == "n"){
-        console.log("Deveria aprender");
-        process.exit();
-    }else{
-        console.log("Qual?");
-        
-    }
-    
-    
-        
+process.stdin.on("data", function (data) {
+  let entrada = data.toString().trim();
+  let instrumentos = data.toString().trim();
 
+  if (entrada == "sim" || entrada == "s") {
+    console.log("Qual?");
+  } else if (instrumentos == "guitarra") {
+    console.log("Você é bom com os dedos");
+    process.exit();
+  } else if (instrumentos == "trompete") {
+    console.log("Quanto fôlego");
+    process.exit();
+  } else if (instrumentos == "bateria") {
+    console.log("Você tem um bom ritmo");
+    process.exit();
+  } else {
+    console.log("Deveria aprender");
+    process.exit();
+  }
 });
-
