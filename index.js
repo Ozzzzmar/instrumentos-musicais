f97124e0577c32753c34bceb07c1366cfded0d99
Osmar <juninhomachado579@gmail.com>
2024-06-13 11:19:27 -0500

    pequena formatação

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Você toca algum instrumento?");
 
 process.stdin.on("data", function (data) {
-  let entrada = data.toString().trim();
+  let entrada = data.toString().trim().toLowerCase();
   let instrumentos;
 
   if (entrada == "sim" || entrada == "s") {
@@ -10,15 +10,12 @@ process.stdin.on("data", function (data) {
     instrumentos = entrada;
     if (instrumentos == "guitarra") {
         console.log("Você é bom com os dedos");
-        process.exit();
     } else if (instrumentos == "trompete") {
         console.log("Quanto fôlego");
-        process.exit();
     } else if (instrumentos == "bateria") {
-        console.log("Você tem um bom ritmo");
-        process.exit();
+        console.log("Você tem um bom ritmo");  
     }
-   process.exit();
+    process.exit();
   } else {
     console.log("Deveria aprender");
     process.exit();
