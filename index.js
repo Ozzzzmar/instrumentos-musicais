d2534e1a7274102e27a950a1aa9be6a86929d12d
Osmar <juninhomachado579@gmail.com>
2024-06-13 10:41:22 -0500

    código formatado

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ console.log("Você toca algum instrumento?");
 
 process.stdin.on("data", function (data) {
   let entrada = data.toString().trim();
-  let instrumentos = data.toString().trim();
+  let instrumentos;
 
   if (entrada == "sim" || entrada == "s") {
     console.log("Qual?");
-  } else if (instrumentos == "guitarra") {
-    console.log("Você é bom com os dedos");
-    process.exit();
-  } else if (instrumentos == "trompete") {
-    console.log("Quanto fôlego");
-    process.exit();
-  } else if (instrumentos == "bateria") {
-    console.log("Você tem um bom ritmo");
-    process.exit();
+  } else if(!instrumentos){
+    instrumentos = entrada;
+    if (instrumentos == "guitarra") {
+        console.log("Você é bom com os dedos");
+        process.exit();
+    } else if (instrumentos == "trompete") {
+        console.log("Quanto fôlego");
+        process.exit();
+    } else if (instrumentos == "bateria") {
+        console.log("Você tem um bom ritmo");
+        process.exit();
+    }
+   process.exit();
   } else {
     console.log("Deveria aprender");
     process.exit();
